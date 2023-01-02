c608523e8946722fcbc89598a2be797d519026d6
CYD <295645351@qq.com>
2023-01-02 22:20:53 +0800

    feat: 预算编辑&删除功能

--- frontend/src/views/budget/Index.vue
+++ frontend/src/views/budget/Index.vue
@@ -31,18 +31,60 @@
         <div class="tips">预算：{{ item.budget }}</div>
         <div class="line budget"></div>
         <div class="tips">花费：{{ item.cost }}</div>
-        <div class="line cost" :style="{ width: `${item.costPercent * 100}%`, background: item.costPercent !== 1 ? '#33e392' : 'red' }"></div>
+        <div class="line cost">
+          <div
+            :style="{
+              height: '100%',
+              width: `${item.costPercent * 100}%`,
+              background: item.costPercent !== 1 ? '#33e392' : 'red'
+            }"
+          ></div>
+        </div>
+        <div class="operate">
+          <el-button size="mini" type="primary" icon="el-icon-edit" circle @click="showEdit(item)"></el-button>
+          <el-button size="mini" type="danger" icon="el-icon-delete" circle @click="showDelete(item)"></el-button>
+        </div>
       </div>
+      <!-- 编辑预算弹窗 -->
+      <el-dialog title="编辑预算" custom-class="edit-budget-dialog" :visible.sync="editDialog">
+        <el-form :model="editBudgetObj" ref="editForm" :rules="rules" class="form-container" labelPosition="top">
+          <el-form-item prop="costTypeId" required size="mini" label="支出类型">
+            <el-select style="width: 100%" disabled v-model="editBudgetObj.costTypeId" filterable placeholder="请选择" clearable>
+              <el-option v-for="(item, index) in costTypes" :key="item + '_' + index" :label="item.name" :value="item.id"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item prop="budget" required size="mini" label="金额">
+            <el-input v-model="editBudgetObj.budget" clearable placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item class="form-item" size="mini">
+            <el-button type="primary" :disabled="btnLoading" @click="submitEditBudget">保存</el-button>
+            <el-button size="small" :disabled="btnLoading" @click="editDialog = false">取消</el-button>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
+      <!-- 删除弹窗 -->
+      <el-dialog title="您确定要删除吗？" custom-class="delete-budget-dialog" :visible.sync="deleteDialog">
+        <div style="color: #999">删除后数据将无法恢复</div>
+        <div slot="footer" class="dialog-footer">
+          <el-button size="small" :loading="btnLoading" type="primary" @click="submitDeleteBudget">确定</el-button>
+          <el-button size="small" :disabled="btnLoading" @click="deleteDialog = false">取消</el-button>
+        </div>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-import { getBudgetList, addBudget, deleteBudget } from './api'
+import { getBudgetList, addBudget, editBudget, deleteBudget } from './api'
 export default {
   data() {
     return {
+      editDialog: false, // 编辑弹窗
+      editBudgetObj: {}, // 编辑预算
+      btnLoading: false, // 按钮loading
+      deleteDialog: false, // 删除弹窗
+      deleteObj: null, // 删除对象
       totalBudget: '0', // 总预算
       totalCost: '0', // 总支出
       year: '', // 当前年度
@@ -88,6 +130,22 @@ export default {
       budgetDetails: [], // 预算明细列表
       rules: {
         money: [
+          { required: true, message: '请输入金额', trigger: 'change' },
+          {
+            validator: (rule, value, callback) => {
+              const num = Number(value)
+              if (isNaN(num)) {
+                callback(new Error('请输入数字'))
+              } else if (num <= 0) {
+                callback(new Error('金额必须为正数'))
+              } else {
+                callback()
+              }
+            },
+            trigger: 'blur'
+          }
+        ],
+        budget: [
           { required: true, message: '请输入金额', trigger: 'change' },
           {
             validator: (rule, value, callback) => {
@@ -158,7 +216,6 @@ export default {
     addBudget() {
       this.$refs.addForm.validate(async (valid) => {
         if (valid) {
-          alert('submit!')
           const data = { date: this.year, billId: this.billId, costTypeId: this.addInformation.costTypeId, money: this.addInformation.money }
           const [err, res] = await addBudget({ data })
           if (err) return
@@ -175,6 +232,52 @@ export default {
     },
     initEcharts() {
       this.chart.setOption(this.options, true)
+    },
+    // 显示编辑弹窗
+    showEdit(item) {
+      this.editBudgetObj = JSON.parse(JSON.stringify(item))
+      this.editDialog = true
+    },
+    // 编辑预算
+    submitEditBudget() {
+      this.$refs.editForm.validate(async (valid) => {
+        if (valid) {
+          const data = { date: this.year, billId: this.billId, id: this.editBudgetObj._id, money: this.editBudgetObj.budget }
+          this.btnLoading = true
+          const [err, res] = await editBudget({ data })
+          this.btnLoading = false
+          this.editDialog = false
+          if (err) return
+          if (res.retCode === 0) {
+            this.$message.success('编辑预算成功')
+            this.getDetail()
+          } else {
+            this.$message.error('编辑预算失败，' + res.message)
+          }
+        } else {
+          return false
+        }
+      })
+    },
+    // 显示删除弹窗
+    showDelete(item) {
+      this.deleteObj = item
+      this.deleteDialog = true
+    },
+    // 删除预算
+    async submitDeleteBudget() {
+      const data = { date: this.year, billId: this.billId, id: this.deleteObj._id }
+      this.btnLoading = true
+      const [err, res] = await deleteBudget({ data })
+      this.btnLoading = false
+      if (err) return
+      this.deleteDialog = false
+      if (res.retCode === 0) {
+        this.$message.success('删除预算成功')
+        this.getDetail()
+      } else {
+        this.$message.error('删除预算失败，' + res.message)
+      }
     }
   }
 }
@@ -233,8 +336,8 @@ export default {
     flex-wrap: wrap;
     // background-color: #fafafa;
     .budget-list-card-item {
-      width: 200px;
-      height: 150px;
+      width: 250px;
+      height: 200px;
       border-radius: 4px;
       box-shadow: 1px 1px 5px #ddd;
       padding: 10px;
@@ -258,7 +361,26 @@ export default {
         border-radius: 10px;
         background: red;
       }
+      .cost {
+        background-color: gray;
+        overflow: hidden;
+        margin-bottom: 20px;
+      }
+      .operate {
+        text-align: center;
+      }
     }
+  }
+}
+</style>
+<style lang="scss">
+.delete-budget-dialog {
+  width: 380px;
+}
+.edit-budget-dialog {
+  width: 400px;
+  .form-item {
+    text-align: center;
   }
 }
 </style>

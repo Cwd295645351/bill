0e556410458b5c76092e071678e2ead49f203db2
CYD <295645351@qq.com>
2023-02-06 23:42:05 +0800

    feat: 概览模块---30%

--- frontend/src/views/overview/Index.vue
+++ frontend/src/views/overview/Index.vue
@@ -3,8 +3,35 @@
     <div class="belong-user-module">
       <div class="title">收支概览</div>
       <div class="total-context">
-        <div class="line">总支出：{{ belongUsers.totalCost }}</div>
-        <div class="line">总收入：{{ belongUsers.totalIncomes }}</div>
+        <div class="line">
+          总支出：<span class="color-red">{{ belongUsers.totalCost }}</span>
+        </div>
+        <div class="line">
+          总收入：<span class="color-red">{{ belongUsers.totalIncomes }}</span>
+        </div>
+      </div>
+      <div class="detail">
+        <div class="detail-item" v-for="item in belongUsers.details" :key="item.belongUserId + '_' + item.userId">
+          ({{ item.userName }}) {{ item.belongUserName }}支出：<span class="color-red">{{ item.money.toFixed(2) }}</span>
+        </div>
+      </div>
+      <div class="add-data-container">
+        <el-form :model="belongCondition" ref="form" class="form-container" labelPosition="top">
+          <el-form-item prop="startDate" required size="small" label="开始时间">
+            <el-date-picker style="width: 100%" v-model="belongCondition.startDate" value-format="yyyy-MM-dd" type="date" placeholder="开始时间">
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item prop="endDate" required size="small" label="结束时间">
+            <el-date-picker style="width: 100%" v-model="belongCondition.endDate" value-format="yyyy-MM-dd" type="date" placeholder="结束时间">
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item prop="proportion" required size="small" label="比例（宜：栋）">
+            <el-input v-model="belongCondition.proportion" placeholder="比例（宜：栋）"></el-input>
+          </el-form-item>
+          <el-form-item class="form-item" size="small">
+            <el-button type="primary" @click="search">查询</el-button>
+          </el-form-item>
+        </el-form>
       </div>
     </div>
   </div>
@@ -16,8 +43,62 @@ export default {
     return {
       belongUsers: {
         totalCost: 0, // 总支出
-        totalIncomes: 0 // 总收入
+        totalIncomes: 0, // 总收入
+        details: [
+          {
+            belongUserId: '63023d593783325f04872f00',
+            belongUserName: '栋',
+            money: 142,
+            userId: '63023d593783325f04872f00',
+            userName: '栋'
+          },
+          {
+            belongUserId: '630cd5db5d09bb41002461bb',
+            belongUserName: '宜',
+            money: 142,
+            userId: '63023d593783325f04872f00',
+            userName: '栋'
+          },
+          {
+            belongUserId: '',
+            belongUserName: '全部',
+            money: 142,
+            userId: '63023d593783325f04872f00',
+            userName: '栋'
+          },
+          {
+            belongUserId: '630cd5db5d09bb41002461bb',
+            belongUserName: '宜',
+            money: 142,
+            userId: '630cd5db5d09bb41002461bb',
+            userName: '宜'
+          },
+          {
+            belongUserId: '63023d593783325f04872f00',
+            belongUserName: '栋',
+            money: 142,
+            userId: '630cd5db5d09bb41002461bb',
+            userName: '宜'
+          },
+          {
+            belongUserId: '',
+            belongUserName: '全部',
+            money: 142,
+            userId: '630cd5db5d09bb41002461bb',
+            userName: '宜'
+          }
+        ]
+      },
+      belongCondition: {
+        startDate: '',
+        endDate: '',
+        proportion: '954.44:1055'
       }
+    }
+  },
+  methods: {
+    search() {
+      console.log(this.belongCondition)
     }
   }
 }
@@ -31,6 +112,9 @@ export default {
   display: flex;
   align-items: center;
   padding: 20px;
+  .color-red {
+    color: rgb(247, 79, 79);
+  }
   .belong-user-module {
     width: 300px;
     height: 100%;
@@ -38,20 +122,41 @@ export default {
     box-shadow: 1px 1px 5px #999;
     background-color: #fff;
     padding: 20px;
+    display: flex;
+    flex-direction: column;
     .title {
-      font-size: 18px;
+      font-size: 26px;
       font-weight: bold;
       text-align: center;
-      height: 30px;
-      line-height: 30px;
+      // height: 30px;
+      // line-height: 30px;
       margin-bottom: 20px;
     }
     .total-context {
       padding-bottom: 10px;
       border-top: 1px solid #ddd;
       border-bottom: 1px solid #ddd;
+      font-size: 24px;
       .line {
         padding-top: 10px;
+      }
+    }
+    .detail {
+      flex: 1;
+      min-height: 0;
+      .detail-item {
+        height: 35px;
+        line-height: 35px;
+        font-size: 20px;
+        border-bottom: 1px dotted #ccc;
+      }
+    }
+    .add-data-container {
+      width: 100%;
+      height: 320px;
+      border-top: 1px solid #ddd;
+      .form-item {
+        text-align: center;
       }
     }
   }

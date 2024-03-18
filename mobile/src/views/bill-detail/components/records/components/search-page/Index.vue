9b8eb80787bcaf32916afaadd211ff432c97f95b
Cwd295645351 <295645351@qq.com>
2024-03-18 22:46:49 +0800

    feat: 账本详情模块--记账模块--------45%

--- mobile/src/views/bill-detail/components/records/components/search-page/Index.vue
+++ mobile/src/views/bill-detail/components/records/components/search-page/Index.vue
@@ -3,14 +3,17 @@
   <!-- 右侧弹出 -->
   <van-popup v-model:show="showSearchCondition" position="right" :style="{ width: '70%', height: '100%' }">
     <van-form @submit="onSubmit">
-      <van-field v-model="searchOptions.userId" is-link readonly name="picker" label="记账人" placeholder="点击选择记账人" @click="showPicker = true" />
-      <van-popup v-model:show="showPicker" position="bottom">
-        <van-picker :columns="users" :columns-field-names="{ text: 'name', value: 'id' }" @confirm="onSelectUser" @change="onSelectUser" @cancel="showPicker = false" />
-      </van-popup>
+      <van-field v-model="searchOptions.userName" is-link readonly name="picker" label="记账人" placeholder="点击选择记账人" @click="showPickerAction('user')" />
+      <van-field v-model="searchOptions.costTypeName" is-link readonly name="picker" label="支出类型" placeholder="点击选择支出类型" @click="showPickerAction('costType')" />
+      <van-field v-model="searchOptions.payMethodName" is-link readonly name="picker" label="支付方式" placeholder="点击选择支付方式" @click="showPickerAction('payMethod')" />
+
       <div style="margin: 16px">
         <van-button round block type="primary" native-type="submit"> 提交 </van-button>
       </div>
     </van-form>
+    <van-popup v-model:show="showPicker" position="bottom">
+      <van-picker :columns="currentPickerOptions.options" :columns-field-names="{ text: 'name', value: 'id' }" @confirm="onConfirmPicker" @cancel="showPicker = false" />
+    </van-popup>
   </van-popup>
 </template>
 
@@ -19,15 +22,11 @@ import { Ref, ref } from 'vue'
 import { useSearch } from './hooks/use-search'
 const showSearchCondition = ref(false)
 
-const { searchOptions, users, costTypes, incomesTypes, payMethods, conditionOptions } = useSearch()
+const { searchOptions, showPicker, currentPickerOptions, showPickerAction, onConfirmPicker } = useSearch()
 
-const onSelectUser = ({ selectedValues, selectedOptions, selectedIndexes }) => {
-  searchOptions.value.userId = selectedValues[0]
+const onSubmit = () => {
+  console.log(searchOptions.value)
 }
-
-const showPicker = ref(false)
-
-const onSubmit = () => {}
 </script>
 
 <style scoped></style>
--- mobile/src/views/bill-detail/components/records/components/search-page/hooks/use-search.ts
+++ mobile/src/views/bill-detail/components/records/components/search-page/hooks/use-search.ts
@@ -6,50 +6,76 @@ import type { Bill, User, CostType, IncomesType, PayMethod } from '@/types/bill'
 export const useSearch = () => {
   const store = useBillStore()
   const { billId, bill } = storeToRefs(store)
+
+  /** 显示选择器 */
+  const showPicker = ref(false)
+  /** 当前选择器配置 */
+  const currentPickerOptions = ref({
+    type: '',
+    options: [],
+  })
+
   /** 查询条件 */
-  const searchOptions = ref({
+  const searchOptions: any = ref({
     beginDate: '', // 开始时间
     endDate: '', // 结束时间
-    userId: '', // 记账人id
-    belongUserId: '', // 归属人id
-    costTypeId: null, // 支出类型
+    userId: '', // 记账人 id
+    userName: '', // 记账人名称
+    belongUserId: '', // 归属人 id
+    belongUserName: '', // 归属人名称
+    costTypeId: null, // 支出类型 id
+    costTypeName: null, // 支出类型
     incomesTypeId: '', // 收入类型
-    payMethodId: '', // 支付方式
+    incomesTypeName: null, // 支出类型
+    payMethodId: '', // 支付方式 id
+    payMethodName: '', // 支付方式
     remark: '', // 内容
-    condition: 'gte',
+    conditionId: 'gte',
+    conditionName: 'gte',
     rangeMoney: 0,
   })
 
-  /** 记账人配置项 */
-  const users = ref<User[]>([])
-  /** 支出类型配置项 */
-  const costTypes = ref<CostType[]>([])
-  /** 收入类型配置项 */
-  const incomesTypes = ref<IncomesType[]>([])
-  /** 支付方式配置项 */
-  const payMethods = ref<PayMethod[]>([])
-
-  /** 查询金额条件 */
-  const conditionOptions = ref([
-    { name: '>=', id: 'gte' },
-    { name: '>', id: 'gt' },
-    { name: '<=', id: 'lte' },
-    { name: '<', id: 'lt' },
-  ])
+  const pickerOptionsMap: any = ref({
+    /** 记账人配置项 */
+    user: [],
+    /** 支出类型配置项 */
+    costType: [],
+    /** 收入类型配置项 */
+    incomeType: [],
+    /** 支付方式配置项 */
+    payMethod: [],
+    condition: [
+      { name: '>=', id: 'gte' },
+      { name: '>', id: 'gt' },
+      { name: '<=', id: 'lte' },
+      { name: '<', id: 'lt' },
+    ],
+  })
 
   /** 初始化数据 */
   const init = () => {
     const billInfo = bill.value as Bill
-    users.value = billInfo.users
-    costTypes.value = billInfo.costTypes
-    incomesTypes.value = billInfo.incomesTypes
-    payMethods.value = billInfo.payMethods
+    const options = pickerOptionsMap.value
+    options.user = billInfo.users
+    options.costType = billInfo.costTypes
+    options.incomesType = billInfo.incomesTypes
+    options.payMethod = billInfo.payMethods
   }
 
-  /** 重置数据 */
-  const resetData = () => {}
+  const showPickerAction = (type: 'user' | 'costType' | 'incomeType' | 'payMethod') => {
+    currentPickerOptions.value.options = pickerOptionsMap.value[type]
+    currentPickerOptions.value.type = type
+    showPicker.value = true
+  }
 
+  const onConfirmPicker = ({ selectedOptions }) => {
+    const idKey = `${currentPickerOptions.value.type}Id`
+    const nameKey = `${currentPickerOptions.value.type}Name`
+    searchOptions.value[idKey] = selectedOptions[0].id
+    searchOptions.value[nameKey] = selectedOptions[0].name
+    showPicker.value = false
+  }
   onMounted(init)
 
-  return { searchOptions, users, costTypes, incomesTypes, payMethods, conditionOptions }
+  return { searchOptions, showPicker, currentPickerOptions, showPickerAction, onConfirmPicker }
 }
